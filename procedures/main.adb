e090cc6ea03d5d290658de244cba3ae22d20dff7
weremax <hardcode45@yahoo.com>
2026-01-28 06:29:44 -0500

    updated procedures

--- procedures/main.adb
+++ procedures/main.adb
@@ -3,10 +3,30 @@ with Ada.Text_IO;
 use Ada.Text_IO;
 
 procedure Main is
+   X : Integer;
+   Y : Integer;
+
    procedure Say_Hello(Name: String) is
    begin
       Put_Line ("hello, " &Name);
    end Say_Hello;
+
+   procedure Swap(A, B : in out Integer) is
+      Temp : Integer;
+      begin
+         Temp := A;
+         A := B;
+         B := Temp;
+      end Swap;
+      
 begin
    Say_Hello("Ada");
+
+   X := 1;
+   Y := 2;
+
+   Swap(X, Y);
+
+   Put_Line(Integer'Image(X));
+   Put_Line(Integer'Image(Y));
 end Main;
